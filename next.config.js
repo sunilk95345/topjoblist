a9a050c9b2b6a2d1c9d70ee9557b3482337466b7
sunilk95345 <137388245+sunilk95345@users.noreply.github.com>
2023-06-28 15:08:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
-
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/:slug',
+        destination: 'https://lifelinejob.com/:slug', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ]
+  },
+}
 module.exports = nextConfig
